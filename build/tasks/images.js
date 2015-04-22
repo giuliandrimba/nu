483b0989676d8f1ed3e9ab124263c9dfbcfe7396
Giulian Drimba <giuliandrimba@gmail.com>
2015-04-22 15:27:07 -0300

    Update images.js

--- build/tasks/images.js
+++ build/tasks/images.js
@@ -2,14 +2,15 @@ var gulp = require("gulp");
 var imagemin = require("gulp-imagemin");
 var cache = require("gulp-cache");
 var gulpFilter = require('gulp-filter');
+var debug = require('gulp-debug');
 
 gulp.task('images', function() {
 
-  var filter = gulpFilter(['*', '!icons']);
+  var filter = gulpFilter(['*', '**/*', '!icons']);
 
-  return gulp.src('../src/images/**/*')
+  return gulp.src(['../src/images/**/*'])
     .pipe(filter)
+    .pipe(debug())
     .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('../public/images'));
 });
-
