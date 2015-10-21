4d7c4db15b3878351298c77ceceaab4693c900bf
Giulian Drimba <giuliandrimba@gmail.com>
2015-10-20 23:55:14 -0200

    clear cache

--- build/tasks/clean.js
+++ build/tasks/clean.js
@@ -1,8 +1,11 @@
 var gulp = require("gulp");
 var del = require("del");
+var cache = require("gulp-cache");
 
 var config = require("../config.json")
 
 gulp.task('clean', function(cb) {
-    del([config.output.path + "/" + config.output.styles, config.output.path + "/" + config.output.scripts, config.output.path + "/images"], {force:true}, cb)
+	cache.clearAll(function() {
+	    del([config.output.path + "/" + config.output.styles, config.output.path + "/" + config.output.scripts, config.output.path + "/images"], {force:true}, cb)
+	});
 });
