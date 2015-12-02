da819be5a359076d99d52dc00e676a3a0f2af001
Giulian Drimba <giuliandrimba@gmail.com>
2015-12-02 15:00:29 -0200

    Update browserify watch task

--- build/tasks/browserify.js
+++ build/tasks/browserify.js
@@ -8,16 +8,11 @@ var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var server = require("./server");
-var sourcemaps = require('gulp-sourcemaps');
 var config = require("../config.json");
-var coffeeify = require("coffeeify");
-var babelify = require("babelify");
 var jadeify = require("jadeify");
 var stringify = require("stringify");
-var watchify = require('watchify');
 
 var env = argv.env != "production";
-var env = true
 
 gulp.task('browserify', function() {
 
@@ -26,24 +21,20 @@ gulp.task('browserify', function() {
     packageCache: {},
     entries: config.browserify.boot,
     debug: env,
-    extensions: ['.coffee', '.jade', '.html'],
-    plugin: [watchify]
+    extensions: ['.coffee', '.jade', '.html']
   })
   .transform("coffeeify")
   .transform("babelify", {presets: ["es2015"]})
   .transform(jadeify)
   .transform(stringify(['.html']));
 
-  b.on('update', bundle);
-
   function bundle() {
     b.bundle()
     .on('error', gutil.log)
     .pipe(source("app.js"))
     .pipe(buffer())
-    .pipe(rename('app.js'))
     .pipe(argv.env != "production" ? gutil.noop() : gStreamify(uglify()))
-    .pipe(gulp.dest('../public'))
+    .pipe(gulp.dest(config.output.path))
     .pipe(server.refresh());
   }
 
--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -5,5 +5,5 @@ gulp.task('watch', ["build", "serve"], function() {
   gulp.watch(config.sprites.src, ['sprites']);
   gulp.watch(config.images.src, ['images']);
   gulp.watch([config.styles.path + '/**/*.styl'], ['styles']);
-  gulp.watch([config.browserify.path + '/**/*.coffee', config.browserify.path + '/**/*.jade', config.browserify.path + '/**/*.js'], ['browserify']);
+  gulp.watch([config.browserify.path + '/**/*.coffee', config.browserify.path + '/**/*.jade', config.browserify.path + '/**/*.js', config.browserify.path + '/**/*.html'], ['browserify']);
 });
